e6c4df1b40827f331618f2abd432b2397e8855c9
GustavoPilar <pilargustavo7@gmail.com>
2024-06-19 18:14:19 -0300

    Diferença entre Operadores de Igualdade

--- 02 - condicionais/aula1.js
+++ 02 - condicionais/aula1.js
@@ -3,6 +3,13 @@
 
 const numero = 10;
 
-const NumeroPar = (numero % 2) == 0;
+const NumeroPar = (numero % 2) === 0;
+/* Diferenças entre operadores de igualdade
+
+    == é igual, ou seja, se eu colocar '0', por mais que o resultado da conta seja um número, ele está igual ao texto, então retornará True
+
+    === é igual igual, ou seja, se estiver dentro de uma string como no exemplo acima, '0', ele vai retornar False, por ser uma string e o resultado ser um inteiro. Então ele verifica o tipo dessa igualdade.
+    
+*/
 
 console.log(NumeroPar);
