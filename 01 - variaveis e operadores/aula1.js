dbba6e8dad997011f168eee8c51a44b816cdf5e7
GustavoPilar <pilargustavo7@gmail.com>
2024-06-19 17:36:33 -0300

    Criando o repositório para o curso de JS

---
+++ 01 - variaveis e operadores/aula1.js
@@ -0,0 +1,8 @@
+
+// Variável em JS
+let variavel = 10;
+
+// Constante em JS
+const pi = 3.14;
+
+console.log('Olá');
---
+++ 01 - variaveis e operadores/aula2.js
@@ -0,0 +1,17 @@
+
+let variavel = 10 + 10;
+
+console.log(variavel)
+
+
+
+/*  
+    const constante = 5;
+
+    console.log(constante)
+
+    //erro de compilação, constante não pode mudar
+    const constante = 6;
+
+    console.log(constante);
+*/
