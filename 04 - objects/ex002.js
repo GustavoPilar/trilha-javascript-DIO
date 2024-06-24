5a3f4386f067dc8346dc1852dbc6c16f3d5772d4
GustavoPilar <pilargustavo7@gmail.com>
2024-06-24 15:10:51 -0300

    Adicionando função de classificação de imc

--- 04 - objects/ex002.js
+++ 04 - objects/ex002.js
@@ -17,13 +17,27 @@ class Pessoas {
     }
 
     dizerIMC() {
-        console.log('Meu IMC é de ', calcularIMC(this.peso, this.altura));
+        console.log('Meu IMC é de ', calcularIMC(this.peso, this.altura), ". ", classificarIMC(calcularIMC(this.peso, this.altura)));
     }
 }
 
 function calcularIMC(peso, altura) {
     const IMC = peso / Math.pow(altura, 2);
     return IMC.toFixed(2);
+}
+
+function classificarIMC(imc) {
+    if (imc < 18.5) {
+        return 'Abaixo do peso.';
+    } else if (imc <= 25) {
+        return 'Peso normal.';
+    } else if (imc <= 30) {
+        return 'Acima do peso.';
+    } else if (imc <= 40) {
+        return 'Obeso';
+    } else {
+        return 'Obesidade Grave.';
+    }
 }
 
 const jose = new Pessoas('José', 1.75, 70);
